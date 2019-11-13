11be1b6044d498c3206b258b3bd5f46d832d5d2d
Raul Herranz <raul.herranz@cazoo.co.uk>
2019-11-13 14:08:19 +0000

    updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setup(
   name = 'cazoo_cloudwatch_events',
   packages = ['cazoo_cloudwatch_events'],
-  version = '0.0.2',
+  version = '0.1.7',
   license='MIT',
   description = 'Cazoo common library for Cloudwatch Events',
   long_description=long_description,
